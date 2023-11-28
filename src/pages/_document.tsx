b9a2517f5e3ed08cd7c83814051e23842a5c7d3f
kamite0611 <kamiteakira@gmail.com>
2023-11-28 21:32:16 +0900

    save favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,7 +22,27 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
   return (
     <Html lang="ja">
       <Head>
-        {/* TODO: Generated https://realfavicongenerator.net/ */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_package/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_package/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_package/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_package/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         {emotionStyleTags}
       </Head>
       <body>
